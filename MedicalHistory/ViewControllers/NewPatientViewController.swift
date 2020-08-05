bbffd828e12a9ee39c9e1a4a7556847daac41c88
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-08-05 17:22:57 +1000

    Create new Collection view and corresponding cell to insert new patient name

--- MedicalHistory/ViewControllers/NewPatientViewController.swift
+++ MedicalHistory/ViewControllers/NewPatientViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class NewPatientViewController: UIViewController {
     
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var newPatientDataSource = NewPatientDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+        newPatientDataSource.collectionView = collectionView
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
 }
